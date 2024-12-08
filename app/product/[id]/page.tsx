e8eb12850b273f86755a8dd992812038dfd1670f
ValeriaPostylyakova <valeria.postylyakova@mail.ru>
2024-12-08 17:14:21 +0300

    создание отдельной страницы продукта

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,3 +1,9 @@
+import {
+    Container,
+    GroupVariants,
+    ProductImage,
+    Title,
+} from '@/components/shared';
 import { prisma } from '@/prisma/prisma-client';
 import { notFound } from 'next/navigation';
 
@@ -10,7 +16,32 @@ const ProductPage = async ({ params: { id } }: { params: { id: string } }) => {
 
     if (!product) notFound();
 
-    return <div>ProductPage {id}</div>;
+    return (
+        <Container className="flex flex-col my-10">
+            <div className="flex flex-1">
+                <ProductImage imageUrl={product.imageUrl} size={30} />
+                <div className="w-[490px] bg-[#f7f6f5] p-7">
+                    <Title
+                        text={product.name}
+                        size="md"
+                        className="font-extrabold m-1"
+                    />
+                    <p className="text-gray-400">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Consequuntur, quae!
+                    </p>
+                    <GroupVariants
+                        items={[
+                            { name: 'Маленькая', value: '1', disabled: true },
+                            { name: 'Средняя', value: '2' },
+                            { name: 'Большая', value: '3' },
+                        ]}
+                        value="2"
+                    />
+                </div>
+            </div>
+        </Container>
+    );
 };
 
 export default ProductPage;
